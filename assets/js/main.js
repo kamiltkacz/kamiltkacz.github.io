b0a639e4162dc14dd8041d27c1cc388da2ed49cd
Kamil Tkacz <kamiltkacz@gmail.com>
2021-07-12 17:43:58 -0300

    var 2,3,4

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,30 +96,94 @@
 
 // Get the modal
 var modal = document.getElementById("myModal");
+var modal_2 = document.getElementById("myModal_2");
+var modal_3 = document.getElementById("myModal_3");
+var modal_4 = document.getElementById("myModal_4");
+
 
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
+var btn_2 = document.getElementById("myBtn_2");
+var btn_3 = document.getElementById("myBtn_3");
+var btn_4 = document.getElementById("myBtn_4");
+
 
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+var span_2 = document.getElementsByClassName("close_2")[0];
+var span_3 = document.getElementsByClassName("close_3")[0];
+var span_4 = document.getElementsByClassName("close_4")[0];
+
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
+
+
 }
+
+btn_2.onclick = function() {
+	modal_2.style.display = "block";
+
+  }
+
+  btn_3.onclick = function() {
+	modal_3.style.display = "block";
+
+  }
+
+  btn_4.onclick = function() {
+	modal_4.style.display = "block";
+
+  }
+
+
+
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+	modal.style.display = "none";
+
+
 }
+
+span_2.onclick = function() {
+
+	modal_2.style.display = "none";
+
+}
+
+span_3.onclick = function() {
+
+	modal_3.style.display = "none";
+
+}
+
+span_4.onclick = function() {
+
+	modal_4.style.display = "none";
+
+}
+
+
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+
+  if (event.target == modal_2) {
+    modal_2.style.display = "none";
+  }
+
+  if (event.target == modal_3) {
+    modal_3.style.display = "none";
+  }
+
+
 }
 
 
